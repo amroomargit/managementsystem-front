7615fbb56238d0668b39456dd9d50694468da9c9
Amro Omar <amroomargit@gmail.com>
2025-11-28 07:42:08 -0500

    Created Angular frontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Admin } from './admin/admin';
+import { Student } from './student/student';
+import { Teacher } from './teacher/teacher';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'login',
+    component: Login
+  },
+  {path:'admin',
+    component:Admin
+  },
+  {path:'teacher',
+    component:Teacher
+  },
+  {path:'student',
+    component:Student
+  }];
