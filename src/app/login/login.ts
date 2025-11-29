d5302510e7628ab20f9f5440be65e6e478659499
Amro Omar <amroomargit@gmail.com>
2025-11-29 16:36:15 -0500

    Login complete

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
+  constructor(private http: HttpClient, private router: Router){}
+
+  login(){
+    const body = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.http.post('http://localhost:8081/users/login',body)
+    .subscribe({
+      next: (response: any) => {
+        console.log("Success");
+        alert("Success")
+        localStorage.setItem('token',response.token); //Store token
+        /*this.router.navigate(['/dashboard']);*/
+      },
+      error: () => {
+        this.errorMessage = 'Invalid username or password.';
+      }
+    });
+  }
 }
