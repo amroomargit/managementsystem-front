858c74ff60659e63e7a7446940f5791881d93d21
Amro Omar <amroomargit@gmail.com>
2025-12-02 17:00:55 -0500

    minor tweak

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -5,6 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class Auth {
   private token: string | null = null; //When we get tokens put them here
+  private authenticatedUser: boolean = false; //Variable holding whether user was authenticated or not (in case of a page reload where everything is lost to avoid logging in again)
+
 
   saveToken(token: string){
     localStorage.setItem('jwt',token);
@@ -17,4 +19,16 @@ export class Auth {
   clearToken(): void {
     localStorage.removeItem('jwt');
   }
+
+  setAuthenticatedUser(authenticatedUser : boolean){
+    this.authenticatedUser = authenticatedUser;
+  }
+
+  getAuthenticatedUser(): boolean{
+    return this.authenticatedUser;
+  }
+
+  setFirstname(){
+  }
+
 }
