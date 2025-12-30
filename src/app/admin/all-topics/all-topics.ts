5d2af583c8c365454c65d1694a30a10dbe2f8fb8
Amro Omar <amroomargit@gmail.com>
2025-12-30 14:37:30 -0500

    Topic delete finally solved

--- src/app/admin/all-topics/all-topics.ts
+++ src/app/admin/all-topics/all-topics.ts
@@ -10,6 +10,7 @@ import { Popup } from '../../popup/popup';
 import { SimplifiedGridViewPopup } from '../../simplified-grid-view-popup/simplified-grid-view-popup';
 import { InsertStudentPopup } from '../../insert-student-popup/insert-student-popup';
 import { CourseDTO } from '../../models/course-dto';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-all-topics',
@@ -21,8 +22,17 @@ export class AllTopics {
   private http = inject(HttpClient);
   private dialog = inject(MatDialog)
   public backendService = inject(BackendService);
+  snackbar = inject(MatSnackBar);
 
   public topics: Observable<TopicDTO[]> | null = null;
+
+
+  snackbarMessage(msg:string){
+    this.snackbar.open(msg,"Close",{
+      duration:10000,
+      panelClass:['snackbar-error']
+    });
+  }
 
   ngOnInit(){
     this.backendService.loadAllTopics();
@@ -54,22 +64,16 @@ export class AllTopics {
 
     dialogRef.afterClosed().subscribe(result=>{
       if(result === "Yes"){
-        this.http.delete<CourseDTO[]>(`http://localhost:8081/topics/delete-topic/${topicId}`)
-        .subscribe((courses) =>
-          {
-
-            /*For every course that lost it's topic (which we know from the list of CourseDTOs that was returned from the backend after doing the topic delete)
-            we need to update those courses, since in the real world, it would be impossible to teach a course that has no topic (though our code actually does
-            allow this, it's for practicality reasons (we need to imagine how confused a user would be if they could see courses with no topic)*/
-            const updateRequests = courses.map(course => this.http.put(`http://localhost:8081/courses/update-course/${course.id}`,course));
-
-            //forkJoin helps us avoid racing by making it so that the courses and topics only reload after we finish with the updates
-            forkJoin(updateRequests).subscribe(()=>{
-              this.backendService.loadAllCourses();
-              this.backendService.loadAllTopics();
-            });
-          });
-        };
+        this.http.delete(`http://localhost:8081/topics/delete-topic/${topicId}`)
+        .subscribe({
+          next: () =>{
+            this.backendService.loadAllTopics();
+          },
+          error: err => {
+            this.snackbarMessage(err.error.message);
+          }
+        });
+      };
     });
   }
 
